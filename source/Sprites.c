31d56218c0551b256e89d5cc9325787a613c3e90
MotionControls <bprogers43@tntech.edu>
2023-10-24 10:30:46 -0500

    Rewrote functions in Sprites.c to be more descriptive.

--- source/Sprites.c
+++ source/Sprites.c
@@ -1,12 +1,12 @@
 #include "Sprites.h"
 
-void InitSprite(int sb, const u16* t, int tL, const u16* p, int pL){
-    memcpy16(&tile_mem[4][sb], t, tL/2);    // Tiles
-	memcpy16(pal_obj_mem, p, pL/2);         // Palette
+void InitSprite(int scrBlock, const u16* tiles, int tLen, const u16* pal, int pLen){
+    memcpy16(&tile_mem[4][scrBlock], tiles, tLen/2);    // Tiles
+	memcpy16(pal_obj_mem, pal, pLen/2);         		// Palette
 }
 
-void InitBackground(const u16* t, int tL, const u16* p, int pL, const u16* m, int mL){
-    memcpy16(&tile_mem[0][0], t, tL/2); // Tiles
-	memcpy16(pal_bg_mem, p, pL/2);      // Palette
-	memcpy16(&se_mem[30][0], m, mL/2);  // Map
+void InitBackground(const u16* tiles, int tLen, const u16* pal, int pLen, const u16* map, int mLen){
+    memcpy16(&tile_mem[0][0], tiles, tLen/2);	// Tiles
+	memcpy16(pal_bg_mem, pal, pLen/2);      	// Palette
+	memcpy16(&se_mem[30][0], map, mLen/2);  	// Map
 }
