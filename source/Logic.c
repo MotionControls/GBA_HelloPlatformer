cc89c7496c1b28fa72374225bf6ce34a311b787c
MotionControls <bprogers43@tntech.edu>
2023-10-27 19:25:59 -0500

    Init commit of Logic Init and Step functions.

--- source/Logic.c
+++ source/Logic.c
@@ -1,1 +1,18 @@
 #include "Logic.h"
+
+void InitLogic(){
+    oam_init(Warden.oamArray, OAM_MAX);
+    
+    Warden = (O_WARDEN){
+        .oamCount = 0,
+        .curLevel = 0,
+    };
+
+    InitPlayer(0,0);
+}
+
+void StepLogic(){
+    StepPlayer();
+
+    oam_copy(oam_mem, Warden.oamArray, Warden.oamCount);
+}
--- source/Logic.h
+++ source/Logic.h
@@ -1,8 +1,11 @@
 #ifndef LOGIC_H
 #define LOGIC_H
 
+#include <tonc.h>
 #include "Defs.h"
+#include "Player.h"
 
 void InitLogic();
+void StepLogic();
 
 #endif
