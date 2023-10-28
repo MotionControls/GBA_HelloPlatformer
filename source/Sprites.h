870bdace5161e3d80739ad0a8d1b4dd61f703381
MotionControls <bprogers43@tntech.edu>
2023-10-27 19:26:41 -0500

    Included Sprite files.

--- source/Sprites.h
+++ source/Sprites.h
@@ -4,6 +4,10 @@
 #include <tonc.h>
 #include "string.h"
 
+#include "sprites/S_Player_Idle.h"
+#include "sprites/S_Player_Run.h"
+#include "sprites/S_Player_Skid.h"
+
 void InitSprite(int spriteBank, const u16* tiles, int tilesLen, const u16* palette, int paletteLen);
 void InitBackground(const u16* tiles, int tilesLen, const u16* pal, int palLen, const u16* map, int mapLen);
 
