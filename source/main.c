5232b5229670214c9b664babaf958dd1830aff72
MotionControls <bprogers43@tntech.edu>
2023-10-27 19:26:31 -0500

    Called logic functions in main.

--- source/main.c
+++ source/main.c
@@ -1,19 +1,17 @@
 #define ISMAIN
 
-#include <string.h>
 #include <stdlib.h>
-#include <math.h>
 
-#include <tonc.h>
-
-#include "Defs.h"
-#include "Sprites.h"
 #include "Logic.h"
 
 int main(){
+	InitLogic();
+	
 	REG_DISPCNT = DCNT_BG0 | DCNT_OBJ_1D | DCNT_OBJ;
 	while(1){
 		vid_vsync();
 		key_poll();
+
+		StepLogic();
 	}
 }
